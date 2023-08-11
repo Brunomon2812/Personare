83081618c24437137d8260621326bb651762041c
Bruno Monteiro <104601906+Brunomon2812@users.noreply.github.com>
2023-08-11 15:00:31 -0400

    Components Created

---
+++ personare/src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+
+export default class Button extends Component {
+    render() {
+        return (
+            <div>
+                <button>Iniciar</button>
+            </div>
+        )
+    }
+}
---
+++ personare/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                Tarot
+            </div>
+        )
+    }
+}
